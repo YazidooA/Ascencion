63c70be4cb1b8224dabbce42e4088db64ba69391
JB0601 <jean-baptiste.guerimand@gmail.com>
2025-02-23 16:24:19 +0100

    remlace tout

---
+++ Ascension/GAME/Assets/CollisionController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionController : MonoBehaviour
+{
+    [SerializeField] public Collision myCollision;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Shop")
+        {
+            SceneManager.LoadScene("Scene_interface_shop");
+            Debug.Log("Collision with Shop");
+        }
+    }
+}
+
