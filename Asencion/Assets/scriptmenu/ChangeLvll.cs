23656fce082bc8374ca626088c5f25acde915869
maxime.elophe <maxime.elophe@epita.fr>
2025-03-02 19:10:32 +0100

    20

--- Asencion/Assets/scriptmenu/ChangeLvll.cs
+++ Asencion/Assets/scriptmenu/ChangeLvll.cs
@@ -9,4 +9,29 @@ public class ChangeLvll : MonoBehaviour
     {
         SceneManager.LoadScene("Level1");
     }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+    public void Retour()
+    {
+        SceneManager.LoadScene("menuscene");
+    }
+
 }
