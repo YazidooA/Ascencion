7fa18433a2000872f89273fd93ad7fe16abc9951
JB0601 <jean-baptiste.guerimand@gmail.com>
2025-03-02 17:59:48 +0100

    maj

---
+++ Asencion/Assets/ChangeScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeSceneBaseCamp()
+    {
+        SceneManager.LoadScene("Scene_Camps_de_bases");
+    }
+}
