b3187f97026d0a081c8a9c61a103558322b01c03
maxime.elophe <maxime.elophe@epita.fr>
2025-03-02 18:06:18 +0100

    17

---
+++ Asencion/Assets/scriptmenu/ChangeLvll.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangeLvll : MonoBehaviour
+{
+    public void  Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
